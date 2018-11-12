2cad0ce450ff54ce393b58bd8eea2d4e70dd32ad
rogerp67 <roger@cambit.nl>
2018-11-12 17:15:01 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+﻿"# homebridge-yamaha_mc2" 
+Based on the Yamaha Extended Control API Spec (https://github.com/samvdb/php-musiccast-api/blob/master/yxc-api-spec-advanced.pdf)
+With this homebridge module you can control your Yamaha Networked devices, in my case MusicCast devices. 
+This homebridge module is an alternative for homebridge-yamaha_mc. Since I configured it as a lightbulb you can also set teh volume (brightness :) )
+beside setting it on and off. This because the Apple Homekit does not support speakers yet. Just name your device as speaker or what you like, in Homekit.
+Enjoy!
+ 
+ 
+Add this to the config.json, for each device you want to add. Just adjust the name and the IP address for each accessory. 
+The zone 'main' in general should work, not to be confused with room names.
+For my Yamaha RN-602 the maximum volume setting is 161, for the W-030 and the WX-010 it is 60. 
+Check the max volume setting by using this url in your homenetwork: http://192.168.1.220/YamahaExtendedControl/v1/system/getFeatures (with your own IP of course)
+Find the value at  "range step", volume, max in the JSON string result.
+Add as much speakers you have as a new accessory:
+
+"accessories": [
+        {
+          "accessory": "YamahaMC",
+          "name": "Yamaha RN-602",
+          "host": "192.168.1.210",
+          "zone": "main",
+		  "maxvol":161
+        },
+        {         
+          "accessory": "YamahaMC",
+          "name": "Yamaha WX-030",
+          "host": "192.168.1.220",
+          "zone": "main",
+		  "maxvol":60
+        }   
+]
---
+++ index.js
@@ -0,0 +1,136 @@
+var Service, Characteristic;
+
+const request = require('request');
+const url = require('url');
+ 
+var yamaha;
+
+
+
+module.exports = function(homebridge) {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  
+  homebridge.registerAccessory("homebridge-yamaha_mc", "YamahaMC", Yamaha_mcAccessory);
+}
+
+function Yamaha_mcAccessory(log, config) {
+  this.currentState = false;
+  this.log = log;
+  this.name = config["name"];
+  this.host = config["host"];
+  this.zone = config["zone"];
+  this.maxVol = config["maxvol"];
+}
+
+Yamaha_mcAccessory.prototype = {
+  getServices: function () {
+    let informationService = new Service.AccessoryInformation();
+    informationService
+      .setCharacteristic(Characteristic.Manufacturer, "Cambit")
+      .setCharacteristic(Characteristic.Model, "Yamaha MC")
+      .setCharacteristic(Characteristic.SerialNumber, "6710160330");
+ 
+    let lightbulbService = new Service.Lightbulb("Amplifier");
+    lightbulbService
+      .getCharacteristic(Characteristic.On)
+        .on('get', this.getLightBulbOnCharacteristic.bind(this))
+        .on('set', this.setLightBulbOnCharacteristic.bind(this));
+
+    lightbulbService
+      .getCharacteristic(Characteristic.Brightness) // Volume!!
+        .on('get', this.getLightBulbBrightnessCharacteristic.bind(this))
+        .on('set', this.setLightBulbBrightnessCharacteristic.bind(this));
+
+    this.informationService = informationService;
+    this.lightbulbService = lightbulbService;
+    return [informationService, lightbulbService];
+  },
+  
+  getLightBulbOnCharacteristic: function (next) {
+    const me = this;
+    request({
+        method: 'GET',
+            url: 'http://' + this.host + '/YamahaExtendedControl/v1/' + this.zone + '/getStatus',
+            headers: {
+                'X-AppName': 'MusicCast/1.0',
+                'X-AppPort': '41100',
+			},
+    }, 
+    function (error, response, body) {
+      if (error) {
+        //me.log('HTTP get error ');
+        me.log(error.message);
+        return next(error);
+      }
+	  att=JSON.parse(body);
+	  me.log('HTTP GetStatus result:' + (att.power=='on' ? "On" : "Off"));
+      return next(null, (att.power=='on'));
+    });
+  },
+   
+  setLightBulbOnCharacteristic: function (on, next) {
+    var url='http://' + this.host + '/YamahaExtendedControl/v1/' + this.zone + '/setPower?power=' + (on ? 'on' : 'standby');
+	const me = this;
+    request({
+      url: url  ,
+      method: 'GET',
+      body: ""
+    },
+    function (error, response) {
+      if (error) {
+        //me.log('error with HTTP url='+url);
+        me.log(error.message);
+        return next(error);
+      }
+	  //me.log('HTTP setPower succeeded with url:' + url);
+      return next();
+    });
+  },
+  
+  // speaker characteristics
+  
+  
+  getLightBulbBrightnessCharacteristic: function (next) {
+    const me = this;
+	var res;
+    request({
+        method: 'GET',
+            url: 'http://' + this.host + '/YamahaExtendedControl/v1/' + this.zone + '/getStatus',
+            headers: {
+                'X-AppName': 'MusicCast/1.0',
+                'X-AppPort': '41100',
+			},
+    }, 
+    function (error, response, body) {
+      if (error) {
+        //me.log('HTTP get error ');
+        me.log(error.message);
+        return next(error);
+      }
+	  att=JSON.parse(body);
+	  res = Math.floor(att.volume / this.maxVol * 100);
+	  me.log('HTTP GetStatus result:' + res);
+      return next(null, res);
+    });
+  },
+   
+  setLightBulbBrightnessCharacteristic: function (volume, next) {
+    var url='http://' + this.host + '/YamahaExtendedControl/v1/' + this.zone + '/setVolume?volume=' + Math.floor(volume/100 * this.maxVol);
+	const me = this;
+    request({
+      url: url  ,
+      method: 'GET',
+      body: ""
+    },
+    function (error, response) {
+      if (error) {
+        //me.log('error with HTTP url='+url);
+        me.log(error.message);
+        return next(error);
+      }
+	  //me.log('HTTP setVolume succeeded with url:' + url);
+      return next();
+    });
+  }
+};
